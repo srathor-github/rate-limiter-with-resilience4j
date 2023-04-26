6386e3afd3b2a22ac1b2dcc338d7fa243a2b01dc
Sushant Rathor <srathor.test@gmail.com>
2023-04-26 21:35:44 +1000

    initial commit

---
+++ src/main/java/com/example/ratelimiterwithresilience4j/RateLimiterWithResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratelimiterwithresilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimiterWithResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimiterWithResilience4jApplication.class, args);
+	}
+
+}
